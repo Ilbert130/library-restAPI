d9fb71fafc5675903e6b0fa35e59f9b879f77df9
Ilbert130 <ilbertca27@gmail.com>
2023-04-28 17:14:13 -0400

    Initial configuration made

---
+++ app.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+
+//setting up dotenv
+dotenv.config();
---
+++ dist/app.js
@@ -0,0 +1,10 @@
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+define(["require", "exports", "dotenv"], function (require, exports, dotenv_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    dotenv_1 = __importDefault(dotenv_1);
+    dotenv_1.default.config();
+});
+//# sourceMappingURL=app.js.map
