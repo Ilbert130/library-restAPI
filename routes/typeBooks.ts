d482023cf6d169e661d8d03fd482b46155d62b03
Ilbert130 <ilbertca27@gmail.com>
2023-05-15 15:39:49 -0400

    typebook controller configured in route

--- routes/typeBooks.ts
+++ routes/typeBooks.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { validatorTypeBookDelete, validatorTypeBookGet, validatorTypeBookPost, validatorTypeBookPut } from "../helpers/validators-typeBook";
+import { typeBookDelete, typeBookGet, typeBookPost, typeBookPut, typeBooksGet } from "../controllers/typeBooks";
 
 
 const router = Router();
 
-router.get('/', );
+router.get('/', typeBooksGet);
 
-router.get('/:id', validatorTypeBookGet);
+router.get('/:id', validatorTypeBookGet, typeBookGet);
 
-router.post('/', validatorTypeBookPost);
+router.post('/', validatorTypeBookPost, typeBookPost);
 
-router.put('/:id', validatorTypeBookPut);
+router.put('/:id', validatorTypeBookPut, typeBookPut);
 
-router.delete('/:id', validatorTypeBookDelete);
+router.delete('/:id', validatorTypeBookDelete, typeBookDelete);
 
 export = router;
