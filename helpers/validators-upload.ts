4b1444c5d78de7e6c63ece98034651f894b2a247
Ilbert130 <ilbertca27@gmail.com>
2023-05-16 16:26:14 -0400

    enum colletion created

---
+++ helpers/validators-upload.ts
@@ -0,0 +1,24 @@
+import { check } from "express-validator";
+import { validateFile } from "../middlewares/validate-file";
+import { validateFields } from "../middlewares/validate-fields";
+
+
+const allowcollections = (collection:string, collections:string[]) => {
+
+    const included = collections.includes(collection);
+
+    if(!included) {
+        throw new Error(`The collection ${collection} isn't allow, ${collections}`);
+    }
+
+    return true;
+} 
+
+
+//PUT
+export const validatorUploadPut = [
+    validateFile,
+    check('id', 'It is not a valid id').isMongoId(),
+    check('collection').custom( c => allowcollections(c, [])),
+    validateFields
+];
